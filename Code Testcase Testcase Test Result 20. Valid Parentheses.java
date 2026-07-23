eee4799f49afac1858811e967c3e6f3031d19a10
Mukesh M <mukeshmharish@gmail.com>
2026-07-23 11:01:50 +0530

    Create  Code Testcase Testcase Test Result 20. Valid Parentheses.java

---
+++ Code Testcase Testcase Test Result 20. Valid Parentheses.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character>stack = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){   stack.push(c);}
+            else if(stack.isEmpty() || c == ')' && stack.pop() != '('|| c == ']' && stack.pop() != '['|| c == '}' && stack.pop() != '{'){
+                return false;
+            }   
+        }
+        return stack.isEmpty(); 
+    }
+}
